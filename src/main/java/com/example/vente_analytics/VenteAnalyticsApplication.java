9b31b8b395d27508f030d980b45be3cb12aec53f
jaymz2026 <yabdelmotaleb@outlook.fr>
2026-02-23 13:34:03 +0000

    Upload src/main/java/com/example/vente_analytics/VenteAnalyticsApplication.java

---
+++ src/main/java/com/example/vente_analytics/VenteAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vente_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteAnalyticsApplication.class, args);
+	}
+
+}
